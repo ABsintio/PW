7c032ba99f26ff89c72bff3337d8df8087e2cc4f
ABsintio <absintio098@gmail.com>
2020-04-20 21:45:00 +0200

    Created

---
+++ contatoreServlet/src/a.java
@@ -0,0 +1,52 @@
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+import java.io.*;
+
+public class a extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public a(){}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+        
+        String nome = request.getParameter("nome");
+
+        Cookie[] cookies = request.getCookies();
+        Cookie contatore = cookies[cookies.length - 1];
+        Integer number = Integer.parseInt(contatore.getValue()) + 1;
+        contatore.setValue(String.valueOf(number));
+        response.addCookie(contatore);
+
+        response.setContentType("text/html");
+        PrintWriter html = response.getWriter();
+
+        html.println(
+            "<html>\n" + 
+            "<head>\n" +
+            "<title>Pagina Servlet A</title>\n" +
+            "</head>\n" + 
+            "<body>\n" +
+            "<style>\n" + 
+            "h1 { text-align: center; }\n" + 
+            "h2 { text-align: center; }\n" +
+            "a { text-align: center; }\n" +
+            "</style>\n" +
+            "<h1>Benvenuto nella Servlet A " + (nome == null ? "" : nome) + "</h1>\n" +
+            "<h2>Numero di accesso alla Servlet A o B: " + number + "</h2>\n" + 
+            "<a href=\"index.html\">Cliccare qui per tornare alla pagina di disambiguazione.</a>" +
+            "</body>\n" + 
+            "</html>"
+        );
+
+        
+
+    }
+
+}
---
+++ contatoreServlet/src/disamb.java
@@ -0,0 +1,37 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.*;
+
+public class disamb extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+
+        String url_servlet = request.getParameter("servlet");
+
+        this.checkForCookie(request, response);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url_servlet);
+        dispatcher.forward(request, response);
+
+    }
+
+    private void checkForCookie(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length == 0){
+            response.addCookie(new Cookie("n.accessi", "1"));
+        } else {
+            Cookie cookie = cookies[cookies.length - 1];
+            cookie.setValue(String.valueOf(Integer.parseInt(cookie.getValue()) + 1));
+            response.addCookie(cookie);
+        }
+    }
+
+}
