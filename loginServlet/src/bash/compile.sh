6857353ed4b1b0c6cde8a4456c8f93a9bfafd235
ABsintio <absintio098@gmail.com>
2020-04-20 19:35:04 +0200

    ../loginServlet Message

--- loginServlet/src/bash/compile.sh
+++ loginServlet/src/bash/compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 javac -cp ".:./src/lib/*" -d ./WEB-INF/classes ./src/*.java
 jar cfv ./WEB-INF/lib/servlet.jar ./WEB-INF/classes/*.class
 
 dir=`echo $(pwd) | awk '{split($0, array, "/"); print array[length(array)]}'`
-bash ../script.sh ../$dir
+sudo bash ../script.sh ../$dir $1
