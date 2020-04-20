91a9ca2b51f8782a7a19505372e3c21c945e9979
ABsintio <absintio098@gmail.com>
2020-04-21 01:00:20 +0200

    Inserita directory prova

--- dircreator.sh
+++ dircreator.sh
@@ -18,7 +18,35 @@ do
 	mkdir ./WEB-INF/$j
 done
 
+# Prendo un file qualsiasi web.xml
+path=`find .. -regex '.*web\.xml' | tail -n 1`
+
 touch index.html
+touch ./WEB-INF/web.xml
+
+python3.8 - <<EOF
+import time
+import os
+
+print(os.getcwd())
+
+try:
+	start = time.time()
+	streamr = open("${path}", mode="r")
+	streamw = open("./WEB-INF/web.xml", mode="w")
+	print("Prendo i dati dal file ${path}")
+	while (line := streamr.readline()):
+		if line == "<web-app>\n":
+			break
+		streamw.write(line)
+	print("Scrittura nel file ./$1/WEB-INF/web.xml andata a buon fine")
+except Exception as e:
+	print("Qualcosa è andato storto")
+	print(e)
+finally:
+	end = time.time()
+	print("Procedura completata in {ms} millisecondi".format(ms = str(end - start)))
+EOF
 
 # Sposto anche le librerie
 sudo cp /opt/tomcat/latest/lib/servlet-api.jar ./src/lib/
