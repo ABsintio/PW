7c88857c8f175e07d9a24645d53c03e59683171f
ABsintio <absintio098@gmail.com>
2020-04-20 20:27:09 +0200

    Inserita directory contatoreServlet

---
+++ contatoreServlet/src/bash/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+javac -cp ".:./src/lib/*" -d ./WEB-INF/classes ./src/*.java
+jar cfv ./WEB-INF/lib/servlet.jar ./WEB-INF/classes/*.class
+
+dir=`echo $(pwd) | awk '{split($0, array, "/"); print array[length(array)]}'`
+sudo bash ../script.sh ../$dir $1 $2
