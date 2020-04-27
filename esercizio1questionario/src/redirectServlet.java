e211acd9a5eea2e52b3824d6c6a8f94eef423d4b
ABsintio <absintio098@gmail.com>
2020-04-27 02:46:42 +0200

    Created

---
+++ esercizio1questionario/src/redirectServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class redirectServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+}
