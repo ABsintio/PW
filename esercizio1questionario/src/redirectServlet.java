09a189fad03f530038609acc55d6a77e69d5a210
ABsintio <absintio098@gmail.com>
2020-04-27 23:50:29 +0200

    1

--- esercizio1questionario/src/redirectServlet.java
+++ esercizio1questionario/src/redirectServlet.java
@@ -11,8 +11,16 @@ public class redirectServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+    throws ServletException, IOException {
+
+        HttpSession session = req.getSession(true);
+        req.setAttribute("name", session.getId());
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(
+            "/esercizio1questionario/servletRisposta"
+        );
+        dispatcher.forward(req, resp);
+
     }
 
 }
