b4cb2a08eef2c0efcc8cd62796c733a00e7b4da2
ABsintio <absintio098@gmail.com>
2020-06-03 22:13:27 +0200

    Create

---
+++ TagPersonalizzati/src/headinTag.jsp
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Heading Tag</title>
+</head>
+<%@ taglib uri="lib.tld" prefix="formato"%>
+<body>
+    <formato:heading bgColor="#C0C0C0">
+        Default Heading
+    </formato:heading>
+    <hr>
+    <formato:heading bgColor="#EF8429" border="5">
+        Large Bordered Heading
+    </formato:heading>
+</body>
+</html>
---
+++ TagPersonalizzati/src/tags/BodyTagHandler.java
@@ -0,0 +1,49 @@
+package tags;
+
+import javax.servlet.jsp.tagext.*;
+import javax.servlet.jsp.*;
+import java.io.*;
+
+public class BodyTagHandler extends TagSupport {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private String bgColor;  // Un attributo obbligatorio
+    private String border = null;
+
+    public void setBgColor(String bgColor) {
+        this.bgColor = bgColor;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    @Override
+    public int doStartTag() throws JspException{
+        try {
+            JspWriter out = pageContext.getOut();
+            out.println(
+                "<table bgcolor=\"" + this.bgColor + "\"" +
+                this.border != null ? ">" : " border=\"" + this.border + "\">");
+        } catch (IOException ioException) {
+            throw new JspException(ioException.getMessage());
+        }
+        return EVAL_BODY_INCLUDE;
+    }
+
+    @Override
+    public int doEndTag() throws JspException {
+        try {
+            JspWriter out = pageContext.getOut();
+            out.println("</table>");
+        } catch (IOException ioException) {
+            throw new JspException(ioException.getMessage());
+        }
+        return EVAL_PAGE;
+    }
+
+}
